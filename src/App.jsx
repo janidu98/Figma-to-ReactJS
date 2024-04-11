adef62071483ad660373d9a36ed3a9e6db354398
janidu98 <janinduchathushan@gmail.com>
2024-04-11 19:35:20 +0530

    designed header part

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
+import Blog from './components/Blog/Blog'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Info from './components/Info/Info'
+import Testimonials from './components/Testimonials/Testimonials'
+import Topics from './components/Topics/Topics'
 
 function App() {
   
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Header />
+      <Topics />
+      <Info />
+      <Blog />
+      <Testimonials />
+      <Footer />
+    </>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,15 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="wrapper">
+        <div className="cta">
+          <p className="course-name">Sound Design Materclass</p>
+          <h1>Learn the Art of Sound Design</h1>
+          <a href="#" className='demo-btn'>Demo Lesson</a>
+        </div>
+      </div>
+    </header>
   )
 }
 
