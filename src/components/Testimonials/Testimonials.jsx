0caf53211d33691604191a82f5019a9fd4e72960
janidu98 <janinduchathushan@gmail.com>
2024-04-13 01:30:51 +0530

    change style in testimonials section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,11 +1,40 @@
-import './testimonials.css'
+import "./testimonials.css";
 
 const Testimonials = () => {
   return (
-    <div>
-      Testimonials
-    </div>
-  )
-}
+    <section className="dark-gray">
+      <div className="wrapper">
+        <div className="testimonials-container">
+          <h2>What our students say?</h2>
 
-export default Testimonials
+          <div className="contents">
+            <div className="content-info">
+              <img src="../../src/assets/profileImg1.png" alt="profileImg1" />
+              <p className="name">Robert Fox</p>
+              <p className="content-text">
+                I got to learn a lot about Music Production with this course.
+                Thanks :)
+              </p>
+            </div>
+
+            <div className="content-info">
+              <img src="../../src/assets/profileImg2.png" alt="profileImg2" />
+              <p className="name">Emily Smith</p>
+              <p className="content-text">Awesome! Great job!!</p>
+            </div>
+
+            <div className="content-info">
+              <img src="../../src/assets/profileImg3.png" alt="profileImg3" />
+              <p className="name">Peter Adams</p>
+              <p className="content-text">
+                This is a great course. I got to learn a lot.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
