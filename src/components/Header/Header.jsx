5e0fc2ed8cece6b49a3596be22b728fbb03449dd
janidu98 <janinduchathushan@gmail.com>
2024-04-12 17:48:55 +0530

    complete header and navbar section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import Navbar from '../Navbar/Navbar'
 import './header.css'
 
 const Header = () => {
   return (
     <header>
       <div className="wrapper">
+        <Navbar />
         <div className="cta">
           <p className="course-name">Sound Design Materclass</p>
           <h1>Learn the Art of Sound Design</h1>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,27 @@
-import './navbar.css'
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <nav>
+      <a href="#" className="logo">
+        Sound<span className="red">DZign</span>
+      </a>
+      <ul className="list">
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Course Details</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Testimonials</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
