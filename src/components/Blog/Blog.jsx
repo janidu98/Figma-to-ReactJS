65a3534abd547a64a84fe123903f009489b6a548
janidu98 <janinduchathushan@gmail.com>
2024-04-13 00:50:15 +0530

    complete blog section

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import './blog.css'
+import React from "react";
+import "./blog.css";
 
 const Blog = () => {
   return (
-    <div>
-      Blog
-    </div>
-  )
-}
+    <section className="pink">
+      <div className="wrapper">
+        <div className="blog-content">
+          <h2 className="light">Latest Posts</h2>
+          <div className="posts">
+            <div className="post-info">
+              <p className="post-name">DAW</p>
+              <div className="post-content">
+                <img src="../../src/assets/thumbnail1.png" alt="thumbnail1" />
+                <p className="post-text">
+                  How To Use Drum Machine in Logic Pro X
+                </p>
+              </div>
+            </div>
 
-export default Blog
+            <div className="post-info">
+              <p className="post-name">Mixing</p>
+              <div className="post-content">
+                <img src="../../src/assets/thumbnail2.png" alt="thumbnail2" />
+                <p className="post-text">How To Mix Guitars Effectively</p>
+              </div>
+            </div>
+
+            <div className="post-info">
+              <p className="post-name">Vox</p>
+              <div className="post-content">
+                <img src="../../src/assets/thumbnail3.png" alt="thumbnail3" />
+                <p className="post-text">
+                  The Real Power of Harmonies in Music Production
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="post-btn-container">
+            <button className="post-btn">All Posts</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
