6a38eeae485506e008829ee1c718cd9960a83787
janidu98 <janinduchathushan@gmail.com>
2024-04-12 23:41:50 +0530

    complete info section

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -2,9 +2,25 @@ import './info.css'
 
 const Info = () => {
   return (
-    <div>
-      Info
-    </div>
+    <section className='info'>
+      <div className="wrapper">
+        <div className="info-content">
+          <div className="student">
+            <img src="../../src/assets/student.svg" alt="studentImage" />
+            <h3>23,000+</h3>
+            <p>Students</p>
+          </div>
+          <div className="file">
+            <img src="../../src/assets/videofile.svg" alt="fileImage" />
+            <h3>26 Hrs</h3>
+            <p>Video Content</p>
+          </div>
+        </div>
+
+        <img src="../../src/assets/icon2.svg" alt="iconImg2" className='iconImg2'/>
+
+      </div>
+    </section>
   )
 }
 
