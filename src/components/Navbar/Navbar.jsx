ce788c7ccaac1056769a0e2afcb352f98e77dabd
janidu98 <janinduchathushan@gmail.com>
2024-04-15 00:08:22 +0530

    add menu items to navbar for mobile view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,58 @@
+import { useState } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
-  return (
-    <nav>
-      <a href="#" className="logo">
-        Sound<span className="red">DZign</span>
-      </a>
-      <ul className="list">
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Course Details</a>
-        </li>
-        <li>
-          <a href="#">Blog</a>
-        </li>
-        <li>
-          <a href="#">Testimonials</a>
-        </li>
-      </ul>
 
-      <div className="menu-icon">
-        <img src="../../src/assets/menuIcon.svg" alt="menu-icon" />
+  const [isActive,setIsActive] = useState(false);
+
+  return (
+    <>
+      <nav>
+        <a href="#" className="logo">
+          Sound<span className="red">DZign</span>
+        </a>
+        <ul className="list">
+          <li>
+            <a href="#topics">Course Details</a>
+          </li>
+          <li>
+            <a href="#info">About</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#testimonials">Testimonials</a>
+          </li>
+        </ul>
+
+        <div className="menu-icon">
+          <img src="../../src/assets/menuIcon.svg" alt="menu-icon" onClick={() => setIsActive(true)}/>
+        </div>
+      </nav>
+ 
+      <div className={`mobile-menu-container ${isActive ? 'active' : ''}`}>
+        <div className="close-icon">
+          <img src="../../src/assets/closeIcon.svg" alt="close-icon" onClick={() => setIsActive(false)}/>
+        </div>
+
+        <ul className="menu-items">
+          <li>
+            <a href="#topics" onClick={() => setIsActive(false)}>Course Details</a>
+          </li>
+          <li>
+            <a href="#info" onClick={() => setIsActive(false)}>About</a>
+          </li>
+          <li>
+            <a href="#blog" onClick={() => setIsActive(false)}>Blog</a>
+          </li>
+          <li>
+            <a href="#testimonials" onClick={() => setIsActive(false)}>Testimonials</a>
+          </li>
+        </ul>
       </div>
-    </nav>
+      
+    </>
   );
 };
 
